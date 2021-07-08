df68250f4159ca48f06c629559624ec3a2f026af
anthill499 <anthill499@gmail.com>
2021-07-08 16:18:47 -0400

    finished two_sum

--- TDD_development/lib/main.rb
+++ TDD_development/lib/main.rb
@@ -5,4 +5,9 @@ class Array
         hash.keys
     end
 
+    def two_sum
+        new_arr = []
+        self.each_with_index { |el, i| self.each_with_index { |el2, i2| new_arr << [i, i2] if i2 > i && el + el2 == 0 } }
+        new_arr
+    end
 end
--- TDD_development/spec/main_spec.rb
+++ TDD_development/spec/main_spec.rb
@@ -12,6 +12,24 @@ describe Array do
             arr = [1,2,3,1,3]
             expect(arr.my_unique).to_not equal(arr)
         end
+    end
+
+    describe "#two_sum" do
+
+        it "finds all pairs of positions where elements sum to zero" do 
+            arr = [-1, 0, 2, -2, 1]
+            expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+        end
+
+        it "should return pairs where the smaller first elements come first" do
+            arr = [-1, 0, 2, -2, 1]
+            expect(arr.two_sum[0][0]).to be < (arr.two_sum[1][0])
+        end
+
+        it "should return pairs where the smaller second elements come first" do
+            arr = [1, -1, -1]             # [[0, 1][0, 2]]
+            expect(arr.two_sum[0][1]).to be < (arr.two_sum[1][1])
+        end
 
     end
 end
